a050b3b5b3aa9bb4f7616b34cb7e1f0abd6e2f95
Tayyab Ali <tayyabsyed2000@gmail.com>
2023-06-22 13:57:37 +0530

    Backend
    Springboot app initialized

---
+++ Back-End/EmployeeManagementPlatform/src/main/java/com/hostbook/EmployeeManagementPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.hostbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementPlatformApplication.class, args);
+	}
+
+}
