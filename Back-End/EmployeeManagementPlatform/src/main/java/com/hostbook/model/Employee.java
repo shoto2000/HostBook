22a8b9f3496d1f6edf72afbf8a346decedd4a2b1
Tayyab Ali <tayyabsyed2000@gmail.com>
2023-06-22 14:02:28 +0530

    Entity
    Employee class added

---
+++ Back-End/EmployeeManagementPlatform/src/main/java/com/hostbook/model/Employee.java
@@ -0,0 +1,22 @@
+package com.hostbook.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer employeeId;
+    private String employeeFirstName;
+    private String employeeLastName;
+}
