c3e9441cd6314dd12c65966347ad80cbd549bb36
Tayyab Ali <tayyabsyed2000@gmail.com>
2023-06-23 21:46:48 +0530

    Backend
    Swagger

--- Back-End/EmployeeManagementPlatform/src/main/java/com/hostbook/EmployeeManagementPlatformApplication.java
+++ Back-End/EmployeeManagementPlatform/src/main/java/com/hostbook/EmployeeManagementPlatformApplication.java
@@ -2,6 +2,11 @@ package com.hostbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class EmployeeManagementPlatformApplication {
@@ -10,4 +15,7 @@ public class EmployeeManagementPlatformApplication {
 		SpringApplication.run(EmployeeManagementPlatformApplication.class, args);
 	}
 
+	public Docket apis(){
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.hostbook")).build();
+	}
 }
